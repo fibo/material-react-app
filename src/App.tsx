b93af746c9ea039bf66b07430699e54817f935fe
fibo <casati_gianluca@yahoo.it>
2019-12-13 17:58:56 +0100

    Add basic layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import TopAppBar, {
+  TopAppBarFixedAdjust,
+  TopAppBarIcon,
+  TopAppBarRow,
+  TopAppBarSection,
+  TopAppBarTitle
+} from '@material/react-top-app-bar';
+import Drawer, {
+  DrawerAppContent,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle
+} from '@material/react-drawer';
+import MaterialIcon from '@material/react-material-icon';
+import List, {
+  ListItem,
+  ListItemGraphic,
+  ListItemText
+} from '@material/react-list';
+
 import './App.scss';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App () {
+  const [selectedIndex, setSelectedIndex] = useState(0)
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false)
 
-export default App;
+  return (
+    <div className='drawer-container'>
+      <Drawer dismissible open={drawerIsOpen}>
+        <DrawerHeader>
+          <DrawerTitle tag='h2'>
+            jane.smith@gmail.com
+          </DrawerTitle>
+        </DrawerHeader>
+
+        <DrawerContent>
+          <List singleSelection selectedIndex={selectedIndex}>
+            <ListItem>
+              <ListItemGraphic graphic={<MaterialIcon icon='folder'/>} />
+
+              <ListItemText primaryText='Mail' />
+            </ListItem>
+          </List>
+        </DrawerContent>
+      </Drawer>
+
+      <DrawerAppContent className='drawer-app-content'>
+        <TopAppBar>
+          <TopAppBarRow>
+            <TopAppBarSection align='start'>
+              <TopAppBarIcon navIcon tabIndex={0}>
+                <MaterialIcon
+                  hasRipple
+                  icon='menu'
+                  onClick={() => setDrawerIsOpen(!drawerIsOpen)}
+                />
+              </TopAppBarIcon>
+
+              <TopAppBarTitle>Inbox</TopAppBarTitle>
+            </TopAppBarSection>
+          </TopAppBarRow>
+        </TopAppBar>
+
+        <TopAppBarFixedAdjust>
+          Your inbox content
+        </TopAppBarFixedAdjust>
+      </DrawerAppContent>
+    </div>
+  )
+}
