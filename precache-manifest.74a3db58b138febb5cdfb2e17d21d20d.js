180e0c7b3e2fa15da72b142dfe9840e9dfd2d3bf
fibo <casati_gianluca@yahoo.it>
2019-12-14 12:17:00 +0100

    Updates

---
+++ precache-manifest.74a3db58b138febb5cdfb2e17d21d20d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c3e53f2c05bec561f4947dc8e38ba46",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d17b47c15b2bb4bb03d0",
+    "url": "/static/css/main.0b46b214.chunk.css"
+  },
+  {
+    "revision": "29cccf6066f654ef1a86",
+    "url": "/static/js/2.7153ad2d.chunk.js"
+  },
+  {
+    "revision": "5e9bad89e7f2dc9dbb16cc0b13b26ab2",
+    "url": "/static/js/2.7153ad2d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d17b47c15b2bb4bb03d0",
+    "url": "/static/js/main.4923a5c9.chunk.js"
+  },
+  {
+    "revision": "978cbecce23da1d52e0a",
+    "url": "/static/js/runtime-main.faba59bd.js"
+  }
+]);
